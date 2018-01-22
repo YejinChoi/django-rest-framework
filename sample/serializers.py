d409665ab341fa877b79d8b269c66561f168d31a
YejinChoi <cyj95428@naver.com>
2018-01-22 20:47:53 +0900

    Django_rest_framework_cbv

--- sample/serializers.py
+++ sample/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Post
 
+
+#Form과 유사
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
--- sample/urls.py
+++ sample/urls.py
@@ -1,13 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from . import views
+from .views import PostViewSet
 from django.conf.urls import include, url
-from sample.views import post_list, post_detail
 
-#router = DefaultRouter()
-#router.register(r'posts',views.PostViewSet)
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
-    url(r'^posts/$', post_list, name='post-list'),
-    url(r'^posts/(?P<pk>\d+)/$', post_detail, name='post-detail'),
-    #url(r'', include(router.urls))
+    url(r'', include(router.urls)),
 ]

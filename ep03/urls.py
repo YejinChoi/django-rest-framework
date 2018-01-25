23ed42b357f2b52e91a4664c0bae045602a339ff
YejinChoi <cyj95428@naver.com>
2018-01-25 20:41:04 +0900

    PostDetailAPIView/GET_PUT_DELETE 구현

--- ep03/urls.py
+++ ep03/urls.py
@@ -3,4 +3,5 @@ from ep03 import views
 
 urlpatterns = [
     url(r'^post/$',views.PostListAPIView.as_view()),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailAPIView.as_view()),
 ]
--- ep03/views.py
+++ ep03/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostModelSerializer
+from django.shortcuts import get_object_or_404
 
 class PostListAPIView(APIView):
     def get(self, request):
@@ -15,3 +16,26 @@ class PostListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+class PostDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Post, pk=pk)
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostModelSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostModelSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.erros, status=400)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=204)
